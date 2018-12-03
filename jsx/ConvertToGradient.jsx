a118d89bc20c3cca406208611d5fe1e62518856a
Sergey Osokin <32217031+creold@users.noreply.github.com>
2018-12-03 18:05:44 +0500

    upd ConvertToGradient

--- jsx/ConvertToGradient.jsx
+++ jsx/ConvertToGradient.jsx
@@ -17,7 +17,7 @@
 // by shouting me a cup of coffee, you can by via PayPal http://www.paypal.me/osokin/usd
 // ==========================================================================================
 // NOTICE:
-// Tested with Adobe Illustrator CC 2017 (Mac), CS6 (Win).
+// Tested with Adobe Illustrator CC 2017, CC 2018 (Mac), CS6 (Win).
 // This script is provided "as is" without warranty of any kind.
 // Free to use, not for sale.
 // ==========================================================================================
@@ -29,17 +29,19 @@
 #target illustrator
 
 // Global variables
-var scriptName = 'Flat to gradient';
-var doc = app.activeDocument,
+var scriptName = 'Flat to gradient',
+    scriptAuthor = '\u00A9 Sergey Osokin, 2018',
     maxValue = 0,
     fillBad = 0,
     channel = new Array(),
-    gShift, gShiftEnd, gAngle;
+    shiftValue, angleValue, gShiftEnd;
 
 function main() {
     if (app.documents.length == 0) {
         alert('Error: \nOpen a document and try again.');
         return;
+    } else {
+        doc = app.activeDocument;
     }
 
     if (doc.selection.length == 0) {
@@ -56,57 +58,86 @@ function main() {
         channel = ['cyan', 'magenta', 'yellow', 'black'];
     }
 
-    // Enter value
-    gShift = prompt('Enter a value for gradient shift (0-' + maxValue + ')', '0', 'Gradient Shift');
-    if (isNaN(Number(gShift))) {
-        alert('Error: \nPlease enter a numeric value.');
-        return;
-    } else if (gShift === null) {
-        return;
-    } else {
-        gShift = Math.round(gShift);
-        if (gShift <= 0) gShift = 0;
-        if (gShift >= maxValue) gShift = maxValue;
-        gShiftEnd = maxValue - gShift;
+    // Main Window
+    var win = new Window('dialog', scriptName + ' ' + scriptAuthor, undefined);
+    win.orientation = 'row';
+    win.alignChild = ['fill', 'fill'];
+
+    // Value fields
+    var shiftPanel = win.add('panel', undefined, 'Gradient Shift');
+    gShift = shiftPanel.add('edittext', [0, 0, 80, 30], '10');
+    gShift.active = true;
+    var anglePanel = win.add('panel', undefined, 'Gradient Angle');
+    gAngle = anglePanel.add('edittext', [0, 0, 80, 30], '0');
+
+    // Buttons
+    var btns = win.add('group');
+    btns.alignChild = ['fill', 'fill'];
+    btns.orientation = 'column';
+    var ok = btns.add('button', undefined, 'OK', { name: 'ok' });
+    ok.helpTip = 'Press Enter to Run';
+    var cancel = btns.add('button', undefined, 'Cancel', { name: 'cancel' });
+    cancel.helpTip = 'Press Esc to Close';
+
+    ok.onClick = function () {
+        if (isNaN(Number(gShift.text))) {
+            alert('Gradient Shift: \nPlease enter a numeric value.');
+            return;
+        } else if (gShift.text === null) {
+            return;
+        } else {
+            shiftValue = Math.round(Number(gShift.text));
+            if (shiftValue <= 0) shiftValue = 0;
+            if (shiftValue >= maxValue) shiftValue = maxValue;
+            gShiftEnd = maxValue - shiftValue;
+        }
+
+        if (isNaN(Number(gAngle.text))) {
+            alert('Gradient Angle: \nPlease enter a numeric value.');
+            return;
+        } else if (gAngle.text === null) {
+            return;
+        } else {
+            angleValue = Number(gAngle.text);
+        }
+
+        // Start conversion
+        for (var i = 0; i < doc.selection.length; i++) {
+            convertToGradient(doc.selection[i], shiftValue, angleValue);
+        }
+        if (fillBad > 0) {
+            alert('Fill an ' + fillBad + ' objects with flat color. \nAny objects containing gradients, patterns, global colors or empty fills will be omitted.');
+        }
+        win.close();
     }
 
-    gAngle = prompt('Enter a value for gradient angle', '0.0', 'Gradient Angle');
-    if (isNaN(Number(gAngle))) {
-        alert('Error: \nPlease enter a numeric value.');
-        return;
-    } else if (gAngle === null) {
-        return;
-    } else {
-        gAngle = Number(gAngle);
+    cancel.onClick = function () {
+        win.close();
     }
 
-    // Start conversion
-    for (var i = 0; i < doc.selection.length; i++) {
-        convertToGradient(doc.selection[i]);
-    }
-
-    if (fillBad > 0) alert('Fill an ' + fillBad + ' objects with flat color. \nAny objects containing gradients, patterns, spot colors or empty fills will be omitted.');
+    win.center();
+    win.show();
 }
 
 // Search items in selection
-function convertToGradient(obj) {
+function convertToGradient(obj, shift, angle) {
     try {
         switch (obj.typename) {
             case 'GroupItem':
                 for (var j = 0; j < obj.pageItems.length; j++) {
-                    convertToGradient(obj.pageItems[j]);
+                    convertToGradient(obj.pageItems[j], shift, angle);
                 }
                 break;
             case 'PathItem':
                 if (obj.filled == true && chkFillType(obj) == true) {
-                    applyGradient(obj);
+                    applyGradient(obj, shift, angle);
                 } else {
                     fillBad++;
                 }
                 break;
             case 'CompoundPathItem':
                 if (obj.pathItems[0].filled == true && chkFillType(obj.pathItems[0]) == true) {
-                    applyGradient(obj.pathItems[0]);
+                    applyGradient(obj.pathItems[0], shift, angle);
                 } else {
                     fillBad++;
                 }
@@ -118,7 +149,7 @@ function convertToGradient(obj) {
 }
 
 // Apply gradient to items
-function applyGradient(obj) {
+function applyGradient(obj, shift, angle) {
     if (obj.fillColor.typename == 'SpotColor') {
         var currentColor = obj.fillColor.spot.color;
     } else {
@@ -137,22 +168,22 @@ function applyGradient(obj) {
         var startColor = new GrayColor();
         var endColor = new GrayColor();
         var grayColor = Math.round(currentColor.gray);
-        if (grayColor < gShift) startColor.gray = 0;
-        else startColor.gray = grayColor - gShift;
+        if (grayColor < shift) startColor.gray = 0;
+        else startColor.gray = grayColor - shift;
 
-        if (grayColor > (100 - gShift)) endColor.gray = 100;
-        else endColor.gray = grayColor + gShift;
+        if (grayColor > (100 - shift)) endColor.gray = 100;
+        else endColor.gray = grayColor + shift;
     }
 
     //Set color for RGB || CMYK channels
     for (var j = 0; j < channel.length; j++) {
         var channelName = channel[j];
         var originColor = Math.round(currentColor[channelName]);
-        if (originColor < gShift) startColor[channelName] = 0;
-        else startColor[channelName] = originColor - gShift;
+        if (originColor < shift) startColor[channelName] = 0;
+        else startColor[channelName] = originColor - shift;
 
         if (originColor > gShiftEnd) endColor[channelName] = maxValue;
-        else endColor[channelName] = originColor + gShift;
+        else endColor[channelName] = originColor + shift;
     }
 
     // Create a new gradient
@@ -174,7 +205,7 @@ function applyGradient(obj) {
 
     // Apply new gradient to current path item
     obj.fillColor = colorOfGradient;
-    obj.rotate(gAngle, false, false, true, false, Transformation.CENTER);
+    obj.rotate(angle, false, false, true, false, Transformation.CENTER);
 }
 
 function chkFillType(obj) {
